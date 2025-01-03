bca7fa4367f15d0cab29970dc23fd386b7d98317
sortA0329 <tsouta0329@gmail.com>
2025-01-03 10:28:48 +0900

    Update Tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,3 +1,13 @@
 #include <libcpprime/IsPrime.hpp>
 #include <libcpprime/IsPrimeNoTable.hpp>
-int main() {}
+#include <random>
+#include <iostream>
+int main() {
+    int a = 0, b = 0;
+    std::mt19937_64 engine;
+    for (int i = 0; i != 1000000; ++i) {
+        a += cppr::IsPrime(engine());
+        b += cppr::IsPrimeNoTable(engine());
+    }
+    std::cout << a << ' ' << b << std::endl;
+}
