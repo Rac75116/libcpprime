da2e78a1595154ef7adc03d329bd88200bb75ae8
sortA0329 <tsouta0329@gmail.com>
2025-01-05 13:26:12 +0900

    Change the condition of constexpr

--- libcpprime/internal/IsPrimeCommon.hpp
+++ libcpprime/internal/IsPrimeCommon.hpp
@@ -60,7 +60,7 @@
 #pragma intrinsic(_udiv128)
 #endif
 #endif
-#if defined(__cpp_lib_is_constant_evaluated) && defined(__cpp_lib_bitops)
+#if defined(__cpp_lib_is_constant_evaluated) && __cpp_constexpr >= 201907L
 #define LIBCPPRIME_CONSTEXPR constexpr
 #else
 #define LIBCPPRIME_CONSTEXPR
@@ -97,9 +97,15 @@ namespace internal {
         Assume(n != 0);
         return std::countr_zero(n);
 #elif defined(__GNUC__)
-        return __builtin_ctzll(n);
+#ifdef __cpp_lib_is_constant_evaluated
+        if (!std::is_constant_evaluated())
+#endif
+            return __builtin_ctzll(n);
 #elif defined(_MSC_VER)
-        return _tzcnt_u64(n);
+#ifdef __cpp_lib_is_constant_evaluated
+        if (!std::is_constant_evaluated())
+#endif
+            return _tzcnt_u64(n);
 #else
         std::uint64_t x = (n ^ (n - 1)) >> 1;
         x = x - ((x >> 1) & 0x5555555555555555);
@@ -112,16 +118,22 @@ namespace internal {
 #endif
     }
 #if !defined(__cpp_lib_bitops) && !defined(__GNUC__) && !defined(_MSC_VER)
-    LIBCPPRIME_CONSTEXPR const char CountlZeroTable[32] = { 0, 31, 9, 30, 3, 8, 13, 29, 2, 5, 7, 21, 12, 24, 28, 19, 1, 10, 4, 14, 6, 22, 25, 20, 11, 15, 23, 26, 16, 27, 17, 18 };
+    constexpr char CountlZeroTable[32] = { 0, 31, 9, 30, 3, 8, 13, 29, 2, 5, 7, 21, 12, 24, 28, 19, 1, 10, 4, 14, 6, 22, 25, 20, 11, 15, 23, 26, 16, 27, 17, 18 };
 #endif
     LIBCPPRIME_CONSTEXPR std::int32_t CountlZero(std::uint64_t n) noexcept {
 #ifdef __cpp_lib_bitops
         Assume(n != 0);
         return std::countl_zero(n);
 #elif defined(__GNUC__)
-        return __builtin_clzll(n);
+#ifdef __cpp_lib_is_constant_evaluated
+        if (!std::is_constant_evaluated())
+#endif
+            return __builtin_clzll(n);
 #elif defined(_MSC_VER)
-        return _lzcnt_u64(n);
+#ifdef __cpp_lib_is_constant_evaluated
+        if (!std::is_constant_evaluated())
+#endif
+            return _lzcnt_u64(n);
 #else
         std::int32_t offset = 0;
         std::uint32_t x = n >> 32;
@@ -182,7 +194,7 @@ namespace internal {
 #endif
     }
     LIBCPPRIME_CONSTEXPR Int64Pair Divu128(std::uint64_t high, std::uint64_t low, std::uint64_t div) noexcept {
-#if (defined(__GNUC__) || defined(__ICC)) && defined(__x86_64__) && __cpp_constexpr >= 201907L
+#if (defined(__GNUC__) || defined(__ICC)) && defined(__x86_64__)
         if LIBCPPRIME_IF_CONSTEXPR (sizeof(void*) == 8) {
 #ifdef __cpp_lib_is_constant_evaluated
             if (!std::is_constant_evaluated())
@@ -348,7 +360,7 @@ namespace internal {
     };
 
     // clang-format off
-	LIBCPPRIME_CONSTEXPR const std::uint16_t Bases[256] = {
+	constexpr std::uint16_t Bases[256] = {
 1216,1836,8885,4564,10978,5228,15613,13941,1553,173,3615,3144,10065,9259,233,2362,6244,6431,10863,5920,6408,6841,22124,2290,45597,6935,4835,7652,1051,445,5807,842,1534,22140,1282,1733,347,6311,14081,11157,186,703,9862,15490,1720,17816,10433,49185,2535,9158,2143,2840,664,29074,24924,1035,41482,1065,10189,8417,130,4551,5159,48886,
 786,1938,1013,2139,7171,2143,16873,188,5555,42007,1045,3891,2853,23642,148,3585,3027,280,3101,9918,6452,2716,855,990,1925,13557,1063,6916,4965,4380,587,3214,1808,1036,6356,8191,6783,14424,6929,1002,840,422,44215,7753,5799,3415,231,2013,8895,2081,883,3855,5577,876,3574,1925,1192,865,7376,12254,5952,2516,20463,186,
 5411,35353,50898,1084,2127,4305,115,7821,1265,16169,1705,1857,24938,220,3650,1057,482,1690,2718,4309,7496,1515,7972,3763,10954,2817,3430,1423,714,6734,328,2581,2580,10047,2797,155,5951,3817,54850,2173,1318,246,1807,2958,2697,337,4871,2439,736,37112,1226,527,7531,5418,7242,2421,16135,7015,8432,2605,5638,5161,11515,14949,
